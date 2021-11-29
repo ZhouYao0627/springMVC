b134b65a4a68428912089a1f22dce6cefe9049d9
ZhouYao <1607435943@qq.com>
2021-11-29 18:09:49 +0800

    Initial commit

---
+++ springMVC-demo1/src/main/java/com/atguigu/mvc/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.atguigu.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ @author：ZhouYao
+ @create：2021-11-29 13:08
+ */
+
+@Controller
+public class HelloController {
+
+    // "/" --> /WEB-INF/templates/index.xml
+
+    @RequestMapping("/")
+    public String index() {
+        // 返回视图名称
+        return "index";
+    }
+
+    @RequestMapping("/target")
+    public String toTarget(){
+        return "target";
+    }
+
+
+
+}
